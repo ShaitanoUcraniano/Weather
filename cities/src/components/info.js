5e727238b2fb36145e4c60ef17e7ed3862fbf862
ShaitanoUcraniano <Shaitaniuk@gmail.com>
2019-02-10 02:56:39 +0200

    Uploading_files
    Файлы

---
+++ cities/src/components/info.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Info = () => (
+    <div>
+        <h2>Додаток погоди</h2>
+        <p>Дізнайтеся погоду у вашому місті</p>
+    </div>
+)
+
+export default Info;
